07425dc832e8b7b616e52becd296248ad713d509
Alejandro Moreno <alejandromogil@gmail.com>
2024-02-15 19:07:47 -0500

    Refactor BaseEntity to be abstract and add CRUD operations to ProjectsController and ProjectsService

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+
+import { ProjectsService } from './projects.service';
+import { CreateProjectDto } from './dto/create-project';
+import { UpdateProjectDto } from './dto/update-project';
 
 @Controller('projects')
-export class ProjectsController {}
+export class ProjectsController {
+    constructor(private readonly projectsService: ProjectsService) {
+    }
+
+    @Get()
+    async findAll() {
+        return this.projectsService.findAll();
+    }
+
+    @Get(':slug')
+    async findBySlug(@Param('slug') slug: string) {
+        return this.projectsService.findBySlug(slug);
+    }
+
+    @Post()
+    async create(@Body() projectFields: CreateProjectDto) {
+        return this.projectsService.create(projectFields);
+    }
+
+    @Patch(':slug')
+    async update(@Param('slug') slug: string, @Body() projectFields: UpdateProjectDto) {
+        return this.projectsService.update(slug, projectFields);
+    }
+
+    @Delete(':slug')
+    async delete(@Param('slug') slug: string) {
+        return this.projectsService.delete(slug);
+    }
+}
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,4 +1,89 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import slugify from 'slugify';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Project } from './project.entity';
+import { Repository, UpdateResult } from 'typeorm';
+import { CreateProjectDto } from './dto/create-project';
+import { UpdateProjectDto } from './dto/update-project';
 
 @Injectable()
-export class ProjectsService {}
+/**
+ * Service responsible for managing projects.
+ */
+export class ProjectsService {
+    constructor(@InjectRepository(Project) private readonly projectsRepository: Repository<Project>) {
+    }
+
+    /**
+     * Creates a new project.
+     * @param project - The project data.
+     * @returns A promise that resolves to the created project.
+     * @throws HttpException with HttpStatus.CONFLICT if the project already exists.
+     */
+    async create(project: CreateProjectDto): Promise<Project> {
+        const slug = slugify(project.title, { lower: true });
+        const projectExists = await this.projectsRepository.findOne({ where: { slug } });
+
+        if (projectExists) {
+            throw new HttpException('Project already exists', HttpStatus.CONFLICT);
+        }
+
+        const newProject = this.projectsRepository.create({ ...project, slug });
+        return this.projectsRepository.save(newProject);
+    }
+
+    /**
+     * Retrieves all projects.
+     * @returns A promise that resolves to an array of projects.
+     */
+    findAll(): Promise<Project[]> { return this.projectsRepository.find(); }
+
+    /**
+     * Retrieves a project by its slug.
+     * @param slug - The project slug.
+     * @returns A promise that resolves to the project.
+     * @throws HttpException with HttpStatus.NOT_FOUND if the project does not exist.
+     */
+    async findBySlug(slug: string): Promise<Project> {
+        const project = await this.projectsRepository.findOne({ where: { slug } });
+
+        if (!project) {
+            throw new HttpException('Project not found', HttpStatus.NOT_FOUND);
+        }
+
+        return project;
+    }
+
+    /**
+     * Updates a project.
+     * @param slug - The project slug.
+     * @param project - The project data.
+     * @returns A promise that resolves to the updated project.
+     * @throws HttpException with HttpStatus.NOT_FOUND if the project does not exist.
+     */
+    async update(slug: string, project: UpdateProjectDto): Promise<UpdateResult> {
+        const projectExists = await this.projectsRepository.findOne({ where: { slug } });
+        if (!projectExists) {
+            throw new HttpException('Project not found', HttpStatus.NOT_FOUND);
+        }
+
+        const updatedProject = await this.projectsRepository.update({ slug }, project);
+        return updatedProject;
+    }
+
+    /**
+     * Deletes a project.
+     * @param slug - The project slug.
+     * @returns A promise that resolves to the deleted project.
+     * @throws HttpException with HttpStatus.NOT_FOUND if the project does not exist.
+     */
+    async delete(slug: string): Promise<Project> {
+        const project = await this.projectsRepository.findOne({ where: { slug } });
+        if (!project) {
+            throw new HttpException('Project not found', HttpStatus.NOT_FOUND);
+        }
+
+        await this.projectsRepository.delete({ slug });
+        return project;
+    }
+}
