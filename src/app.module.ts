ac9ed28fe1850d64b134655a7e6123cbb0143167
Alejandro Moreno <alejandromogil@gmail.com>
2024-02-16 20:16:50 -0500

    Remove requests module and related files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSourceConfig } from './config/data.source';
 import { ConfigModule } from '@nestjs/config';
+
+import { DataSourceConfig } from './config/data.source';
+
 import { ProjectsModule } from './projects/projects.module';
-import { RequestsModule } from './requests/requests.module';
 import { UsersModule } from './users/users.module';
 import { TeamsModule } from './teams/teams.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRoot(DataSourceConfig), ProjectsModule, RequestsModule, UsersModule, TeamsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRoot(DataSourceConfig), ProjectsModule, UsersModule, TeamsModule],
   controllers: [AppController],
   providers: [AppService],
 })
