6ca4ffdc0c750b11403eb37921aed3c789f2b13c
Alejandro Moreno <alejandromogil@gmail.com>
2024-03-22 20:16:31 -0500

    Allow CORS

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,19 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const reflector = app.get(Reflector);
-  app.useGlobalPipes(new ValidationPipe({
-    transformOptions: {
-      enableImplicitConversion: true,
-    },
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(8000);
 }
 bootstrap();
